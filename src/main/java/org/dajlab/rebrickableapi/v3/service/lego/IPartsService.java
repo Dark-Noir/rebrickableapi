eef794b5ba3ba84b2a88f699b76e561d725ab353
Björn <noir3698@yahoo.de>
2022-02-27 05:17:40 +0100

    Added Parts request methods

--- src/main/java/org/dajlab/rebrickableapi/v3/service/lego/IPartsService.java
+++ src/main/java/org/dajlab/rebrickableapi/v3/service/lego/IPartsService.java
@@ -1,5 +1,40 @@
 package org.dajlab.rebrickableapi.v3.service.lego;
 
+import org.dajlab.rebrickableapi.v3.vo.Part;
+import org.dajlab.rebrickableapi.v3.vo.RebrickableException;
+
+/**
+ * Service for LEGO parts /api/v3/lego/parts/.
+ * 
+ *
+ */
 public interface IPartsService extends IService {
+
+	/**
+	 * Get details about Parts.
+	 * 
+	 * @see <a href=
+	 *      "https://rebrickable.com/api/v3/swagger/#!/lego/lego_parts_list"">lego/parts/</a>
+	 * @param
+	 * @return Parts
+	 * @throws RebrickableException exception
+	 */
+	Part[] getAllParts() throws RebrickableException;
+
+	Part[] getPartByNum(String partNum) throws RebrickableException;
+
+	Part[] getPartByCatId(String catId) throws RebrickableException;
+
+	Part[] getPartByColorId(String colorId) throws RebrickableException;
+
+	Part[] getPartByBricklinkId(String bricklinkId) throws RebrickableException;
+
+	Part[] getPartByBrickowlId(String brickowlId) throws RebrickableException;
+
+	Part[] getPartByLegoId(String legoId) throws RebrickableException;
+
+	Part[] getPartByLdrawId(String ldrawID) throws RebrickableException;
+
+	Part[] getPartBySearch(String search) throws RebrickableException;
 
 }
