79efbec54a1f327fdebf2b58aef57dd0538b5a73
Björn <noir3698@yahoo.de>
2022-05-21 16:51:18 +0200

    Added allPartCategories method

--- src/main/java/org/dajlab/rebrickableapi/v3/service/lego/impl/PartCategoriesServiceImpl.java
+++ src/main/java/org/dajlab/rebrickableapi/v3/service/lego/impl/PartCategoriesServiceImpl.java
@@ -1,7 +1,16 @@
 package org.dajlab.rebrickableapi.v3.service.lego.impl;
 
+import java.util.HashMap;
+
+import org.dajlab.rebrickableapi.v3.enumeration.Method;
 import org.dajlab.rebrickableapi.v3.service.AbstractRebrickableService;
 import org.dajlab.rebrickableapi.v3.service.lego.IPartCategoriesService;
+import org.dajlab.rebrickableapi.v3.vo.PartCategories;
+import org.dajlab.rebrickableapi.v3.vo.RebrickableException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PartCategoriesServiceImpl extends AbstractRebrickableService implements IPartCategoriesService {
 
@@ -17,6 +26,29 @@ public class PartCategoriesServiceImpl extends AbstractRebrickableService implem
 	 */
 	public PartCategoriesServiceImpl(String privateKey) {
 		super(privateKey);
+	}
+
+	@Override
+	public PartCategories[] getAllPartCategories() throws RebrickableException {
+
+		String uri = BASE_URI;
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("page_size", "1000");
+		String output = returnJsonResponse(Method.GET, uri, map);
+		try {
+			if (output != null) {
+				JSONParser parser = new JSONParser();
+				JSONObject json = (JSONObject) parser.parse(output);
+				output = (String) json.get("results").toString();
+
+				ObjectMapper mapper = new ObjectMapper();
+
+				return mapper.readValue(output, PartCategories[].class);
+			}
+		} catch (Exception e) {
+			throw new RebrickableException(e.getMessage());
+		}
+		return null;
 	}
 
 }
